e998afa5e6110658f380879ed99e728cbccd3127
Famous-Soldier <84340939+Famous-Soldier@users.noreply.github.com>
2021-05-17 18:02:46 +0100

    Add files via upload

---
+++ status_change.py
@@ -0,0 +1,28 @@
+import requests, time
+
+token = ""
+u_header = {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+
+red = "dnd" # Red
+idle = "idle" # idle
+green = "online" # Green
+offline = "invisible"
+
+
+requests.patch('https://discord.com/api/v8/users/@me/settings', headers=u_header, json={"status":green})
+
+
+
+
+
+
+
+
+
+
+
+
+
